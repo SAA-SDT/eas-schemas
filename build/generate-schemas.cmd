71827f616657943c39e46e5214e326bb729d6edb
Mark <fordmadox@gmail.com>
2023-12-15 09:20:46 -0500

    just a reminder to make this more flexible for the build process in 2024

--- build/generate-schemas.cmd
+++ build/generate-schemas.cmd
@@ -16,19 +16,21 @@ if %errorlevel%==1 (
 rem still need to update for when we switch between EAD / EAC transformations, which we can do by passing parameters
 rem now just hard-coded for EAD
 
+rem add a flag for the filename postfix, e.g. -4-dev... for now just hardcoded while testing.
+
 @echo Getting started.
 
 java %parameters% %CP% net.sf.saxon.Transform -t -xsl:transformations\prep-source-schema-files.xsl -it
 
 java -jar %jing% -s ..\src\modules\extensible-version\ead\ead-source.rng > ..\xml-schemas\ead\ead.rng
 
-java %parameters% %CP% net.sf.saxon.Transform -s:..\xml-schemas\ead\ead.rng -xsl:transformations\add-comments-and-metadata.xsl -o:..\xml-schemas\ead\ead.rng -warnings:silent
+java %parameters% %CP% net.sf.saxon.Transform -s:..\xml-schemas\ead\ead.rng -xsl:transformations\add-comments-and-metadata.xsl -o:..\xml-schemas\ead\ead-4-dev.rng -warnings:silent
 
 java -jar %trang% -o disable-abstract-elements -o any-process-contents=lax -o any-attribute-process-contents=lax ..\xml-schemas\ead\ead.rng ..\xml-schemas\ead\ead.xsd
 
 java %parameters% %CP% net.sf.saxon.Transform -s:..\xml-schemas\ead\ead.xsd -xsl:transformations\deglobalize-xsd.xsl -o:..\xml-schemas\ead\ead.xsd -warnings:silent
 
-java %parameters% %CP% net.sf.saxon.Transform -s:..\xml-schemas\ead\ead.xsd -xsl:transformations\update-namespace-prefix-in-xsd.xsl -o:..\xml-schemas\ead\ead.xsd -warnings:silent
+java %parameters% %CP% net.sf.saxon.Transform -s:..\xml-schemas\ead\ead.xsd -xsl:transformations\update-namespace-prefix-in-xsd.xsl -o:..\xml-schemas\ead\ead-4-dev.xsd -warnings:silent
 
 @echo All done.
 
