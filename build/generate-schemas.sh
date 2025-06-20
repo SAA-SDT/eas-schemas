63a9eefed76f16975f4bec7613620c4e2ff7592f
Mark <fordmadox@gmail.com>
2025-06-20 09:11:21 -0400

    now generating both eac and ead at the same time

--- build/generate-schemas.sh
+++ build/generate-schemas.sh
@@ -8,21 +8,25 @@ jing="../vendor/jing-20181222/bin/jing.jar"
 trang="../vendor/trang-20091111/trang.jar"
 
 # later, add an option for ALL, ead, eac, or eaf.
+# for now, just run them all, since we want to make sure nothing is / gets broken.
 
-echo "Getting started."
-
-#  pass the parameter here to prep-source-schema-files for each.  for now, i've just switched the file to use 'eac'
-java -cp $saxon net.sf.saxon.Transform -t -xsl:transformations/prep-source-schema-files.xsl -it
-
-#java -jar $jing -s ../src/modules/extensible-version/ead/ead-source.rng > ../xml-schemas/ead/ead-4-dev.rng
+# EAC-CPF
+echo "Getting started. First up, the EAC transformation:"
+java -cp $saxon net.sf.saxon.Transform -t -xsl:transformations/prep-source-schema-files.xsl -it schema='eac'
 java -jar $jing -s ../src/modules/extensible-version/eac/eac-source.rng > ../xml-schemas/eac-cpf/eac.rng
-#java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/ead/ead-4-dev.rng -xsl:transformations/add-comments-and-metadata.xsl -o:../xml-schemas/ead/ead-4-dev.rng
-java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eac-cpf/eac.rng -xsl:transformations/add-comments-and-metadata.xsl -o:../xml-schemas/eac-cpf/eac.rng
-#java -jar $trang -o disable-abstract-elements -o any-process-contents=lax -o any-attribute-process-contents=lax ../xml-schemas/ead/ead-4-dev.rng ../xml-schemas/ead/ead-4-dev.xsd
+java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eac-cpf/eac.rng -xsl:transformations/add-comments-and-metadata.xsl -o:../xml-schemas/eac-cpf/eac.rng schema='eac'
 java -jar $trang -o disable-abstract-elements -o any-process-contents=lax -o any-attribute-process-contents=lax ../xml-schemas/eac-cpf/eac.rng ../xml-schemas/eac-cpf/eac.xsd
-#java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/ead/ead-4-dev.xsd -xsl:transformations/deglobalize-xsd.xsl -o:../xml-schemas/ead/ead-4-dev.xsd
-java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eac-cpf/eac.xsd -xsl:transformations/deglobalize-xsd.xsl -o:../xml-schemas/eac-cpf/eac.xsd
-#java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/ead/ead-4-dev.xsd -xsl:transformations/update-namespace-prefix-in-xsd.xsl -o:../xml-schemas/ead/ead-4-dev.xsd
-java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eac-cpf/eac.xsd -xsl:transformations/update-namespace-prefix-in-xsd.xsl -o:../xml-schemas/eac-cpf/eac.xsd
+java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eac-cpf/eac.xsd -xsl:transformations/deglobalize-xsd.xsl -o:../xml-schemas/eac-cpf/eac.xsd schema='eac'
+java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eac-cpf/eac.xsd -xsl:transformations/update-namespace-prefix-in-xsd.xsl -o:../xml-schemas/eac-cpf/eac.xsd schema='eac'
+
+# EAD
+echo "Next, the EAD transformation:"
+java -cp $saxon net.sf.saxon.Transform -t -xsl:transformations/prep-source-schema-files.xsl -it schema='ead'
+java -jar $jing -s ../src/modules/extensible-version/ead/ead-source.rng > ../xml-schemas/ead/ead-4-dev.rng
+java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/ead/ead-4-dev.rng -xsl:transformations/add-comments-and-metadata.xsl -o:../xml-schemas/ead/ead-4-dev.rng schema='ead'
+java -jar $trang -o disable-abstract-elements -o any-process-contents=lax -o any-attribute-process-contents=lax ../xml-schemas/ead/ead-4-dev.rng ../xml-schemas/ead/ead-4-dev.xsd
+java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/ead/ead-4-dev.xsd -xsl:transformations/deglobalize-xsd.xsl -o:../xml-schemas/ead/ead-4-dev.xsd schema='ead'
+java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/ead/ead-4-dev.xsd -xsl:transformations/update-namespace-prefix-in-xsd.xsl -o:../xml-schemas/ead/ead-4-dev.xsd schema='ead'
+
 
 echo "All done."
