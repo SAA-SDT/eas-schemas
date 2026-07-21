f30a218eff1dd9bd163f63aa3a586c6514ee5ae5
fordmadox <fordmadox@gmail.com>
2026-07-21 09:36:03 -0400

    Revert "update build process to test fixes for attribute issues in objectXMLWrap and formattingExtension"
    This reverts commit 87170ac9949744496b7946e6f8bae4384e11028f.

--- build/generate-schemas.sh
+++ build/generate-schemas.sh
@@ -15,7 +15,6 @@ trang="../vendor/trang-20091111/trang.jar"
 echo "Getting started. First up, the EAC transformation:"
 java -cp $saxon net.sf.saxon.Transform -t -xsl:transformations/prep-source-schema-files.xsl -it schema='eac'
 java -jar $jing -s ../src/modules/extensible-version/eac/eac-source.rng > ../xml-schemas/eac-cpf/eac.rng
-java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eac-cpf/eac.rng -xsl:transformations/revert-jing-namespace-simplification.xsl -o:../xml-schemas/eac-cpf/eac.rng
 java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eac-cpf/eac.rng -xsl:transformations/add-comments-and-metadata.xsl -o:../xml-schemas/eac-cpf/eac.rng schema='eac'
 java -jar $trang -o disable-abstract-elements -o any-process-contents=lax -o any-attribute-process-contents=lax ../xml-schemas/eac-cpf/eac.rng ../xml-schemas/eac-cpf/eac.xsd
 java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eac-cpf/eac.xsd -xsl:transformations/deglobalize-xsd.xsl -o:../xml-schemas/eac-cpf/eac.xsd schema='eac'
@@ -27,7 +26,6 @@ java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eac-cpf/eac.xsd -xsl:tr
 echo "Next, the EAD transformation:"
 java -cp $saxon net.sf.saxon.Transform -t -xsl:transformations/prep-source-schema-files.xsl -it schema='ead'
 java -jar $jing -s ../src/modules/extensible-version/ead/ead-source.rng > ../xml-schemas/ead/ead.rng
-java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/ead/ead.rng -xsl:transformations/revert-jing-namespace-simplification.xsl -o:../xml-schemas/ead/ead.rng
 java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/ead/ead.rng -xsl:transformations/add-comments-and-metadata.xsl -o:../xml-schemas/ead/ead.rng schema='ead'
 java -jar $trang -o disable-abstract-elements -o any-process-contents=lax -o any-attribute-process-contents=lax ../xml-schemas/ead/ead.rng ../xml-schemas/ead/ead.xsd
 java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/ead/ead.xsd -xsl:transformations/deglobalize-xsd.xsl -o:../xml-schemas/ead/ead.xsd schema='ead'
@@ -39,7 +37,6 @@ java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/ead/ead.xsd -xsl:transf
 echo "And last (but not least), the EAF transformation:"
 java -cp $saxon net.sf.saxon.Transform -t -xsl:transformations/prep-source-schema-files.xsl -it schema='eaf'
 java -jar $jing -s ../src/modules/extensible-version/eaf/eaf-source.rng > ../xml-schemas/eaf/eaf.rng
-java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eaf/eaf.rng -xsl:transformations/revert-jing-namespace-simplification.xsl -o:../xml-schemas/eaf/eaf.rng
 java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eaf/eaf.rng -xsl:transformations/add-comments-and-metadata.xsl -o:../xml-schemas/eaf/eaf.rng schema='eaf'
 java -jar $trang -o disable-abstract-elements -o any-process-contents=lax -o any-attribute-process-contents=lax ../xml-schemas/eaf/eaf.rng ../xml-schemas/eaf/eaf.xsd
 java -cp $saxon net.sf.saxon.Transform -s:../xml-schemas/eaf/eaf.xsd -xsl:transformations/deglobalize-xsd.xsl -o:../xml-schemas/eaf/eaf.xsd schema='eaf'
